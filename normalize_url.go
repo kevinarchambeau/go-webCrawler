c9fa58cab62e11361799752f481197ab9eb6a785
kevinarchambeau <xxerexx@gmail.com>
2024-08-23 16:04:44 -0700

    implemented getURLsFromHTML function

--- normalize_url.go
+++ normalize_url.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/url"
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/url"
+	"strings"
+)
 
 func normalizeURL(unparsedURL string) (string, error) {
 	parsedUrl, err := url.Parse(unparsedURL)
@@ -12,4 +17,40 @@ func normalizeURL(unparsedURL string) (string, error) {
 	}
 
 	return parsedUrl.Host + parsedUrl.Path, nil
+}
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	var urls []string
+
+	body := strings.NewReader(htmlBody)
+	htmlNodes, err := html.Parse(body)
+	if err != nil {
+		return urls, fmt.Errorf("failed to get html nodes %v", err)
+	}
+
+	var links func(node *html.Node)
+	links = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, a := range node.Attr {
+				if a.Key == "href" {
+					parsedUrl, err := url.Parse(a.Val)
+					if err != nil {
+						fmt.Println("could not parse as url")
+					}
+					// some incorrect urls can still get through to here, not going to worry about them for now
+					if parsedUrl.Host == "" && parsedUrl.Scheme == "" {
+						urls = append(urls, rawBaseURL+a.Val)
+					} else {
+						urls = append(urls, a.Val)
+					}
+					break
+				}
+			}
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			links(child)
+		}
+	}
+	links(htmlNodes)
+	return urls, nil
 }
