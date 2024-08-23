77e8767484c9fa6357cf04f1a1bfa9ab788a02c5
kevinarchambeau <xxerexx@gmail.com>
2024-08-23 16:18:45 -0700

    implemented main, broke out functions

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args[1:]) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args[1:]) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	baseUrl := os.Args[1:]
+	fmt.Printf("starting crawl of: %s\n", baseUrl[0])
 }
--- normalize_url_test.go
+++ normalize_url_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -37,67 +36,6 @@ func TestNormalizeURL(t *testing.T) {
 			}
 			if actual != tc.expected {
 				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
-			}
-		})
-	}
-}
-
-func TestGetURLsFromHTML(t *testing.T) {
-	tests := []struct {
-		name      string
-		inputURL  string
-		inputBody string
-		expected  []string
-	}{
-		{
-			name:     "absolute and relative URLs",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-			<html>W
-				<body>
-					<a href="/path/one">
-						<span>Boot.dev</span>
-					</a>
-					<a href="https://other.com/path/one">
-						<span>Boot.dev</span>
-					</a>
-				</body>
-			</html>
-			`,
-			expected: []string{"https://blog.boot.dev/path/one", "https://other.com/path/one"},
-		},
-		{
-			name:     "includes other uris",
-			inputURL: "https://blog.boot.dev",
-			inputBody: `
-			<html>
-				<body>
-					<a href="mailto:test@example.com">
-						<span>Boot.dev</span>
-					</a>	
-					<a href="/path/one">
-						<span>Boot.dev</span>
-					</a>
-					<a href="https://other.com/path/one">
-						<span>Boot.dev</span>
-					</a>
-				</body>
-			</html>
-			`,
-			expected: []string{"mailto:test@example.com", "https://blog.boot.dev/path/one", "https://other.com/path/one"},
-		},
-	}
-
-	for i, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			actual, err := getURLsFromHTML(tc.inputBody, tc.inputURL)
-			if err != nil {
-				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
-				return
-			}
-			// ordering matters for DeepEqual
-			if !reflect.DeepEqual(actual, tc.expected) {
-				t.Errorf("Test %v - %s FAIL: expected the following URLs: %v, actual: %v", i, tc.name, tc.expected, actual)
 			}
 		})
 	}
